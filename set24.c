7a294bf258b7cd2aa5632c04912ace86941424e3
Rasigavijaya <46052495+Rasigavijaya@users.noreply.github.com>
2019-02-19 16:05:53 +0530

    Create set24.c

---
+++ set24.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+ char a[50];
+ int i,n,c=0;
+ scanf("%s",a);
+ n=strlen(a);
+ for(i=0;i<n;i++)
+ {
+  if(0<=a[i] && a[i]>=9)
+  {
+   c++;
+  }
+ }
+ if(c==n)
+ {
+  printf("yes");
+ }
+ else
+ {
+  printf("no");
+ }
+}
