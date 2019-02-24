5b16102afad9a7c51a6f5113b4985ed32aa14868
Rasigavijaya <46052495+Rasigavijaya@users.noreply.github.com>
2019-02-24 09:03:36 +0530

    Create set54.c

---
+++ set54.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void main()
+{
+ char a[50],b[50];
+ int i,f=0,n,n1;
+ gets(a);
+ gets(b);
+ n=strlen(a);
+ n1=strlen(b);
+ if(n==n1)
+ {
+  for(i=0;i<n;i++)
+  {
+   if(a[i]!=b[i])
+   {
+    f=1;
+    break;
+   }
+  }
+ }
+ else
+ {
+ printf("no");
+ }
+ if(f==1)
+ {
+  printf("no");
+ }
+ else
+ {
+  printf('yes");
+ }
+ }
