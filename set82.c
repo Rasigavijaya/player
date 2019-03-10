683500ae8d3dd4b9790dd4d24a42b890abb8abcc
Rasigavijaya <46052495+Rasigavijaya@users.noreply.github.com>
2019-03-10 10:49:17 +0530

    Create set82.c

---
+++ set82.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+    int n,a[50],i,m=1;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        m=(m)&(a[i]);
+        
+    }
+    printf("%d",m);
+	return 0;
+}
