583cf15d24c42cd401b823ddfed371b52d859f7c
Rasigavijaya <46052495+Rasigavijaya@users.noreply.github.com>
2019-03-10 08:20:33 +0530

    Create set68.c

---
+++ set68.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,a[50],m=0,c=0,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                c++;
+            }
+        }
+        if(c>=m)
+        {
+            m=c;
+        }
+    }
+    printf("%d",m);
+    return 0;
+}
