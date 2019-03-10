179bcad0af3383f9bc4862ca6c2283b882d651fa
Rasigavijaya <46052495+Rasigavijaya@users.noreply.github.com>
2019-03-10 13:14:20 +0530

    Create set99.c

---
+++ set99.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int n,res,bin=0,t=1;
+	scanf("%d",&n);
+	while(n>=1)
+	{
+	    res=n%10;
+	    bin=res*t+bin;
+	    n=n/10;
+	    t=t*2;
+	}
+	printf("%o",bin);
+	return 0;
+}
